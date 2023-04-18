683b6fb858b7b772769c96f7d7120cd53c4c0877
temamironovonline <a.sergeichev03@mail.ru>
2023-04-18 23:14:41 +0300

    awd

---
+++ KAKNADO/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAKNADO
+{
+    public class Class1
+    {
+        public static string kaknado1(string a)
+        {
+            return a;
+        }
+    }
+}
